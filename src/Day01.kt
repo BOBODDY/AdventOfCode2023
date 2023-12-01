b85b7162abc5c6cc8f552aa1268e53cbbd89cb8c
Nick Mathews <nick.mathews@stablekernel.com>
2023-12-01 13:17:50 -0500

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,88 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val calibration = input.map {
+            it.replace(Regex("[a-zA-Z]"), "")
+        }.map {
+            val num1 = it.first()
+            val num2 = it.last()
+            "$num1$num2"
+        }.map {
+            it.toInt()
+        }
+        return calibration.sum()
+    }
+
+    fun Int.toWord(): String {
+        return when (this) {
+            1 -> "one"
+            2 -> "two"
+            3 -> "three"
+            4 -> "four"
+            5 -> "five"
+            6 -> "six"
+            7 -> "seven"
+            8 -> "eight"
+            9 -> "nine"
+            else -> ""
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        /*
+        First attempt returns 54607:
+        .map { line ->
+            var updatedLine = line
+            var i = 0
+            while (i < updatedLine.length) {
+                val sub = updatedLine.substring(i)
+
+                (1..9).forEach { num ->
+                    val wordForNum = num.toWord()
+                    if (wordForNum.isNotEmpty() && sub.startsWith(wordForNum)) {
+                        updatedLine = updatedLine.replaceFirst(wordForNum, "$num", ignoreCase = true)
+                    }
+                }
+                i += 1
+            }
+            updatedLine
+        }
+         */
+        // Correct implementation: returned 54581
+        val calibration = input
+                .map { line ->
+                    val newStringBuilder = StringBuilder()
+                    line.forEachIndexed { index, char ->
+                        if (char.isDigit()) {
+                            newStringBuilder.append(char)
+                        } else {
+                            val sub = line.substring(index)
+                            (1..9).forEach { num ->
+                                val wordForNum = num.toWord()
+                                if (wordForNum.isNotEmpty() && sub.startsWith(wordForNum)) {
+                                    newStringBuilder.append(num)
+                                }
+                            }
+                        }
+                    }
+                    return@map newStringBuilder.toString()
+                }.map {
+                    val num1 = it.first()
+                    val num2 = it.last()
+                    "$num1$num2"
+                }.map {
+                    it.toInt()
+                }
+        return calibration.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
+    check(part1(testInput) == 142)
+
+    val test2Input = readInput("Day02_test")
+    part2(test2Input).println()
+    check(part2(test2Input) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
